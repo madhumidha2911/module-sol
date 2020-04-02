05e25b4ed0088e8276f65965b3e50d0f0de858b8
madhumidha2911 <60444339+madhumidha2911@users.noreply.github.com>
2020-04-02 09:24:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+(function(){
+"use strict";
+
+angular.module("LunchCheck",[])
+.controller("LunchCheckcontroller",LunchCheckcontroller);
+
+LunchCheckcontroller.$inject=["$scope"];
+
+function LunchCheckcontroller($scope){
+	$scope.dishes="";
+
+	$scope.checkIfTooMuch=function(){
+		var dishes=$scope.dishes;
+
+		dishes=dishes.replace(/[^A-Z0-9]/ig,"");
+
+		var numberofchars = dishes.length;
+
+		if(numberofchars<=0){
+			$scope.message="Empty";
+			return "Empty";
+		}
+
+		dishes=dishes.split(",");
+		var items =dishes.length;
+		if(items>=1 && items<=3){
+			$scope.message="Enjoy!";
+		}
+		else{
+			$scope.message="Too much!"
+		}
+	}
+}
+
+}())
